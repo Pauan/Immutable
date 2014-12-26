5fd590535094172b77fab621523a1452a765e534
Pauan <pcxunlimited@gmail.com>
2014-12-25 18:56:10 -1000

    Create Cons.js

---
+++ src/Cons.js
@@ -0,0 +1,18 @@
+function Cons(car, cdr) {
+  this.car = car;
+  this.cdr = cdr;
+}
+
+Cons.prototype.forEach = function (f) {
+  var self = this;
+  while (self !== nil) {
+    f(self.car);
+    self = self.cdr;
+  }
+};
+
+// TODO this isn't tail recursive
+Cons.prototype.forEachRev = function (f) {
+  this.cdr.forEachRev(f);
+  f(this.car);
+};
