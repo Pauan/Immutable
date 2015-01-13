522a47440a13653e527689b49d75934f5014a0c3
Pauan <pcxunlimited@gmail.com>
2015-01-12 19:31:40 -1000

    The benchmark program now grabs the actual versions, rather than hardcoding them

--- src/Benchmark/run.js
+++ src/Benchmark/run.js
@@ -2,27 +2,31 @@ import * as benchmark from "./Benchmark";
 import * as list from "./List";
 import * as record from "./Record";
 
+var package = require("../package.json");
+
+var dependencies = package.devDependencies;
+
 function header() {
   benchmark.group("Information", function () {
     benchmark.group("Node.js", function () {
       benchmark.message("URL: http://nodejs.org/");
-      benchmark.message("Version: 0.10.22");
+      benchmark.message("Version: " + process.version);
     });
     benchmark.group("Benchmark.js", function () {
       benchmark.message("URL: https://github.com/bestiejs/benchmark.js");
-      benchmark.message("Version: 1.0.0");
+      benchmark.message("Version: " + dependencies.benchmark);
     });
     benchmark.group("Immutable-js", function () {
       benchmark.message("URL: https://github.com/facebook/immutable-js");
-      benchmark.message("Version: 3.4.1");
+      benchmark.message("Version: " + dependencies.immutable);
     });
     benchmark.group("Mori", function () {
       benchmark.message("URL: https://github.com/swannodette/mori");
-      benchmark.message("Version: 0.2.9");
+      benchmark.message("Version: " + dependencies.mori);
     });
     benchmark.group("Immutable", function () {
       benchmark.message("URL: https://github.com/Pauan/Immutable");
-      benchmark.message("Version: 4.0.0");
+      benchmark.message("Version: " + package.version);
     });
     /*benchmark.group("Elm", function () {
       benchmark.message("URL: http://elm-lang.org/");
@@ -32,16 +36,16 @@ function header() {
 }
 
 
-/*header();
+header();
 list.run(10);
 list.run(100);
-list.run(1000);*/
+list.run(1000);
 
-header();
+/*header();
 record.run(1);
 record.run(10);
 record.run(100);
 record.run(1000);
-record.run(10000);
+record.run(10000);*/
 
 benchmark.run();
